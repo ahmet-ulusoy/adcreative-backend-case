dce30b773d40bdf2fe0c04cb496c120ed311a47a
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-07-13 22:44:28 +0300

    Service implementation completed

--- AdCreative.BackendCase/AdCreative.BackendCase/Services/Abstract/IImageDownloadService.cs
+++ AdCreative.BackendCase/AdCreative.BackendCase/Services/Abstract/IImageDownloadService.cs
@@ -2,6 +2,16 @@
 {
     public interface IImageDownloadService
     {
+        int GetNumberOfImagesToDownload();
 
+        int GetMaximumParallelDownloadlimit();
+
+        string GetSavePath();
+
+        void StartToDownloadImages(int numberOfImagesToDownload, int maximumParallelDownloadlimit);
+
+        Task DownloadImagesAsync(int numberOfImagesToDownload, int maximumParallelDownloadlimit, string outputPath, string imageUrl);
+
+        void CancelDownloadImages(string outputPath);
     }
 }
--- AdCreative.BackendCase/AdCreative.BackendCase/Services/Concrete/ImageDownloadService.cs
+++ AdCreative.BackendCase/AdCreative.BackendCase/Services/Concrete/ImageDownloadService.cs
@@ -4,5 +4,36 @@ namespace AdCreative.BackendCase.Services.Concrete
 {
     public class ImageDownloadService : IImageDownloadService
     {
+        public int GetNumberOfImagesToDownload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetMaximumParallelDownloadlimit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSavePath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartToDownloadImages(int numberOfImagesToDownload, int maximumParallelDownloadlimit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DownloadImagesAsync(int numberOfImagesToDownload, int maximumParallelDownloadlimit, string outputPath, string imageUrl)
+        {
+            await Task.CompletedTask;
+
+            throw new NotImplementedException();
+        }
+
+        public void CancelDownloadImages(string outputPath)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
