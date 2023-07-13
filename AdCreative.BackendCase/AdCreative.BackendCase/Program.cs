50725b8f41f665e8b1e09a3ce95b9a9a7b952ce4
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-07-13 22:35:44 +0300

    Create folder structure and update program class

--- AdCreative.BackendCase/AdCreative.BackendCase/Program.cs
+++ AdCreative.BackendCase/AdCreative.BackendCase/Program.cs
@@ -1,10 +1,12 @@
 ﻿namespace AdCreative.BackendCase
 {
-    internal class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            await Task.CompletedTask;
         }
     }
 }
