f7b3b7df67aafd14ce3f2ae2c5d57b689be0bd80
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-07-13 22:47:20 +0300

    Register and resolve service

--- AdCreative.BackendCase/AdCreative.BackendCase/Program.cs
+++ AdCreative.BackendCase/AdCreative.BackendCase/Program.cs
@@ -1,12 +1,43 @@
-﻿namespace AdCreative.BackendCase
+﻿using AdCreative.BackendCase.Services.Abstract;
+using AdCreative.BackendCase.Services.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdCreative.BackendCase
 {
     public static class Program
     {
+        private static ServiceProvider? _serviceProvider;
+
+        private static IImageDownloadService? _imageDownloadService;
+
         public static async Task Main(string[] args)
         {
+            RegisterServices();
+
+            GetImageDownloadService();
+
             Console.WriteLine("Hello, World!");
 
             await Task.CompletedTask;
+        }
+        private static void RegisterServices()
+        {
+            _serviceProvider = new ServiceCollection()
+                .AddSingleton<IImageDownloadService, ImageDownloadService>()
+                .BuildServiceProvider();
+        }
+
+        private static void GetImageDownloadService()
+        {
+            if (_serviceProvider != null)
+            {
+                _imageDownloadService = _serviceProvider.GetRequiredService<IImageDownloadService>();
+
+                if (_imageDownloadService == null)
+                {
+                    throw new InvalidOperationException($"{nameof(_imageDownloadService)} is null.");
+                }
+            }
         }
     }
 }
